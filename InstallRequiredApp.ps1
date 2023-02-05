8ee27471d29a668eba77cdaa7810488cfa814de1
Vikrant Singh <56357625+Visinghr@users.noreply.github.com>
2023-02-05 23:06:39 +0530

    Adding the file for the install required application

---
+++ InstallRequiredApp.ps1
@@ -0,0 +1,29 @@
+﻿$Apps = @(
+"Microsoft.VisualStudioCode",
+"Microsoft.VisualStudio.2022.Enterprise",
+"Microsoft.PowerToys",
+"Oracle.JDK.19",
+"OpenJS.NodeJS.LTS",
+"Git.Git",
+"GitHub.GitHubDesktop",
+"Google.Chrome",
+"KDE.KDiff3",
+"Notepad++.Notepad++",
+"Google.AndroidStudio",
+"Postman.Postman",
+"Python.Python.3.8",
+"Telerik.Fiddler.Classic",
+"Microsoft.WindowsTerminal",
+"Microsoft.PowerShell"
+)
+
+$i = 0
+foreach($app in $Apps){
+    Write-Host "Installation of the application $app started"
+    $i++    
+    $taskCompleted = $i/$Apps.Count*100;
+    Write-Progress -Activity " Applicaton nstallation started ...." -Status "$taskCompleted % completed" -PercentComplete $taskCompleted   
+    winget install --id $app 
+}
+
+Write-Host "Installation Completed "
